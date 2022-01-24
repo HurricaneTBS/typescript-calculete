12967ea64b9016576343dcb200ce2c943a465e35
HurricaneTBS <634092350@qq.com>
2022-01-24 11:55:00 +0800

    2 files modified

--- index.ts
+++ index.ts
@@ -4,3 +4,17 @@ import './style.css';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+const containsDuplicate = (nums: number[]) => {
+  nums.sort((a, b) => a - b);
+  console.log(nums);
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] === nums[i + 1]) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+console.log(containsDuplicate([1,2,3,7,5,6]))
