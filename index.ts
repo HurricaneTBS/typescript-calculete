8e13f87d87bc2f93045cbfe466412d124f8b57e7
HurricaneTBS <634092350@qq.com>
2022-01-24 12:08:04 +0800

    2 files modified

--- index.ts
+++ index.ts
@@ -6,15 +6,17 @@ const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 
 const containsDuplicate = (nums: number[]) => {
-  nums.sort((a, b) => a - b);
-  console.log(nums);
-  for (let i = 0; i < nums.length - 1; i++) {
-    if (nums[i] === nums[i + 1]) {
+  const set = new Set();
+  for(const item of nums){
+    if(set.has(item)){
       return true;
     }
+
+    set.add(item);
   }
 
   return false;
 };
 
-console.log(containsDuplicate([1,2,3,7,5,6]))
+console.log(containsDuplicate([1,2,3,7,5,6,9,0]))
+
