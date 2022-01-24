00cf45c650078f1f85fb1579cca538f2970ab942
HurricaneTBS <634092350@qq.com>
2022-01-24 20:57:24 +0800

    1 file modified, 1 added

--- index.ts
+++ index.ts
@@ -5,17 +5,25 @@ import './style.css';
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 
-const maxSubArray = (nums) => {
-  // 记录第n项时子数组的和f(n)
-  let pre = 0;
-  // 记录最大子数组的和
-  let max = 0;
-  nums.forEach((item) => {
-    pre = Math.max(pre + item, item);
-    max = Math.max(pre, max);
-  });
+const arr = [1, 3, 5, 7, 9, 11];
+const tree = [0];
+const size = arr.length;
 
-  return max;
+const build_tree = (arr, tree, node, start, end) => {
+  if (start === end) {
+    tree[node] = arr[start];
+  } else {
+    const mid = Math.floor((start + end) / 2);
+    const left_node = 2 * node + 1;
+    const right_node = 2 * node + 2;
+
+    build_tree(arr, tree, left_node, start, mid);
+    build_tree(arr, tree, right_node, mid + 1, end);
+    tree[node] = tree[left_node] + tree[right_node];
+  }
+  return tree;
 };
 
-console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
+const result = build_tree(arr, tree, 0, 0, size - 1);
+console.log(result);
+
